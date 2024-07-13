02b001db764f1c440a0be324218c62586bcb4a09
howard0615 <howard159632@gmail.com>
2024-07-13 12:00:02 +0800

    adding description

--- lua/howard/lazy/telescope.lua
+++ lua/howard/lazy/telescope.lua
@@ -11,8 +11,8 @@ return {
         require('telescope').setup({})
 
         local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+        vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = "Find files" })
+        vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = "git_files" })
         vim.keymap.set('n', '<leader>pws', function()
             local word = vim.fn.expand("<cword>")
             builtin.grep_string({ search = word })
@@ -24,7 +24,6 @@ return {
         vim.keymap.set('n', '<leader>ps', function()
             builtin.grep_string({ search = vim.fn.input("Grep > ") })
         end)
-        vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+        vim.keymap.set('n', '<leader>vh', builtin.help_tags, { desc = "help_tags" })
     end
 }
-
